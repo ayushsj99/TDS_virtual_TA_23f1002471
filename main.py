4ed09e98f55fc066d500b8bddd598822f4f260cb
Ayush Jadhav <ayushsjadhav99@gmail.com>
2025-06-16 02:26:36 +0530

    changes in main.py for deployment

--- main.py
+++ main.py
@@ -170,4 +170,4 @@ def get_answer(request: QARequest):
 # ---------- Run Locally ----------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
